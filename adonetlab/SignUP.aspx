9f17ff55830d8f7bcc1efdf5aeb540629287c982
amrita <amritaxtha10@gmail.com>
2023-12-17 08:59:06 +0545

    Add project files.

---
+++ adonetlab/SignUP.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SignUP.aspx.cs" Inherits="adonetlab.SignUP" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width: 100%;">
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="UserName"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="Uname" runat="server"></asp:TextBox></td>
+
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label3" runat="server" Text="Password"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="Password" runat="server"></asp:TextBox></td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td><asp:Label ID="Label2" runat="server" Text="Re-Password"></asp:Label></td>
+                    <td>
+                        <asp:TextBox ID="Repassword" runat="server"></asp:TextBox>
+
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="SignUp" OnClick="Button1_Click"/>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ adonetlab/SignUP.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace adonetlab
+{
+    public partial class SignUP : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs1"].ConnectionString;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "insert into SignUP values(@uname,@password)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@uname", Uname.Text);
+            cmd.Parameters.AddWithValue("@password", Password.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            //try
+            //{
+            //    conn.Open();
+            //    int a = cmd.ExecuteNonQuery();
+
+            //    if (a > 0)
+            //    {
+            //        ClientScript.RegisterStartupScript(typeof(Page), "scripts", "alert('SignUp Successfully.." + Uname.Text + " friend')");
+            //    }
+            //    else
+            //    {
+            //        ClientScript.RegisterStartupScript(this.GetType(), "scripts", "alert('Signup Failed')");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Handle exceptions here
+            //    // You might want to log the exception details or provide a more user-friendly error message
+            //}
+            //finally
+            //{
+            //    conn.Close(); // Always close the connection, whether an exception occurs or not
+            //}
+        
+    }
+    }
+}
